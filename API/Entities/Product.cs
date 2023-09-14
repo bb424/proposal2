20cddb0c4fd9d87a47406956f02cc1a2c10a7149
bb424 <brad9m@yahoo.com>
2023-09-14 13:14:54 -0400

    End of Section 3

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -1,6 +1,6 @@
 namespace API.Entities
 {
-    public class Product
+    public class Product // set up the product database structure
     {
         public int Id { get; set; } //EF will use Id as primary key by default
         public string Name { get; set; }
---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+
+
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, []) /*the square brackets prevent multiple calls which would keep recursively calling requests*/
+
+    
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
+}
